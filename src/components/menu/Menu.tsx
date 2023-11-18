522b993ce1f190adda3c9189e93bcc960d5b02de
Wowaxur <wowaxur@yandex.ru>
2023-11-19 00:30:02 +0300

    add hover scale

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -47,7 +47,9 @@ const StyledMenu = styled.nav`
   li {
     margin-right: 10px;
   }
-
+  li:hover {
+    transform: scale(1.2); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+  }
   @media (max-width: 767px) {
     flex-wrap: wrap;
     align-self: flex-end;
