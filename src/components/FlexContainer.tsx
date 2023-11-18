fc8dbc482aeb93b0c76df16863d533ec8686740b
Wowaxur <wowaxur@yandex.ru>
2023-11-19 00:28:31 +0300

    delete gap

--- src/components/FlexContainer.tsx
+++ src/components/FlexContainer.tsx
@@ -13,7 +13,6 @@ export const FlexContainer = styled.div<FlexContainerPropsType>`
   justify-content: ${props => props.justify || 'flex-start'};
   align-items: ${props => props.align || 'stretch'};
   flex-wrap: ${props => props.wrap|| 'nowrap'}; 
-  gap: 112px;
   @media (max-width: 767px) { display: flex;
     flex-direction: column;
 
