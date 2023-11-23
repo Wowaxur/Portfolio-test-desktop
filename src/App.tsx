15c6f23f07e908e104bb0b16372d59ae1ec18fc4
Wowaxur <wowaxur@yandex.ru>
2023-11-23 18:51:03 +0300

    add BrandsWorksSector

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import React from "react";
 import {GreySector} from "./layout/sections/graySector/GreySector";
 import {Generalsection} from "./layout/sections/generalSection/GeneralSection";
 import {Header} from "./layout/Header/Header";
+import {BrandsWorksSector} from "./layout/sections/brandsWorkSection/BrandsWork";
 function App() {
     return (
         <Generalsection>
             <Header/>
             <MainSection/>
             <GreySector/>
+            <BrandsWorksSector/>
             <Footer/>
         </Generalsection>
     );
