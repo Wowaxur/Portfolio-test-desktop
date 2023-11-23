262f44f3edc947d45a9b7bf7f400826eb382d645
Wowaxur <wowaxur@yandex.ru>
2023-11-23 18:55:31 +0300

    add @media to Rectangle

--- src/components/styledRectangle.tsx
+++ src/components/styledRectangle.tsx
@@ -13,13 +13,18 @@ export const Rectangle = styled.a<StyledRectanglePropsType>`
   background: #2A2A2A;
   border-color: transparent;
   border-style: solid;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.47);
   transition: 1.2s;
   :hover {
     transform: scale(1.2); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
   }
   @media (max-width: 768px) {
-    
+    width: 100px;
+    height: 100px;
+    padding-top: 10px;
   }
 \``
 
