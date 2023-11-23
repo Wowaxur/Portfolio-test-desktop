f376461f335e116d685c175ff6b166857fbb18a5
Wowaxur <wowaxur@yandex.ru>
2023-11-23 18:52:50 +0300

    add new props to FlexContainerPropsType

--- src/components/FlexContainer.tsx
+++ src/components/FlexContainer.tsx
@@ -5,16 +5,23 @@ import styled from "styled-components";
      justify?: string
      align?: string
      wrap?: string
+     bgc?: string
+     margin?: string
+     borRad?: string
 }
 
 export const FlexContainer = styled.div<FlexContainerPropsType>`
  display: flex;
+  border-radius: ${props => props.borRad || 'none'};
+  background-color: ${props => props.bgc || 'none'};
   flex-direction: ${props => props.direction || 'row'};
   justify-content: ${props => props.justify || 'flex-start'};
   align-items: ${props => props.align || 'stretch'};
   flex-wrap: ${props => props.wrap|| 'nowrap'}; 
+  margin: ${props => props.margin|| 'auto'};  ;
   @media (max-width: 767px) { display: flex;
     flex-direction: column;
+    
 
   }
 `
